62df393e5ad1d387fb71d2093c97e34000e8ce69
CrisIntriago <caintria@espol.edu.ec>
2024-04-25 15:23:29 -0500

    merge

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -18,15 +18,15 @@ const Clients = () => {
   return (
     <Slide direction="up" triggerOnce cascade>
       <section className="container mx-auto overflow-hidden pt-14 bg-white mt-10">
-      <div className="mx-auto mb-[60px] max-w-[510px] text-center">
+      <div className="mx-auto mb-[30px] max-w-[510px] text-center">
       <span className="mb-2 block text-lg font-semibold text-mi-naranja">
                 Nuestros Clientes
               </span>
-              <h2 className="mb-3 text-3xl font-bold leading-[1.2] text-dark sm:text-4xl md:text-[40px]">
+              <h2 className="mb-2 text-3xl font-bold leading-[1.2] text-dark sm:text-4xl md:text-[40px]">
                 Principales clientes de nuestra firma
               </h2>
             </div>
-        <div className='mt-10'>
+        <div className='mt-8'>
           <div className='overflow-hidden w-full'>
             <Row speed={20} playing={playing}>
               {clientLogos.map((route, idx) => (
